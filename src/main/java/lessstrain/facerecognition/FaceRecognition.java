23e81e0d99d36efc19c4918b044bc48593ba41c6
kaimu-kun <moser.p2000@gmail.com>
2019-11-23 18:08:53 +0100

    Starting implementing the Face recognition

--- src/main/java/lessstrain/facerecognition/FaceRecognition.java
+++ src/main/java/lessstrain/facerecognition/FaceRecognition.java
@@ -1,58 +1,108 @@
 package lessstrain.facerecognition;
 
-import org.bytedeco.javacv.CanvasFrame;
-import org.bytedeco.javacv.Frame;
-import org.bytedeco.javacv.FrameGrabber;
-import org.bytedeco.javacv.OpenCVFrameGrabber;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.objdetect.CascadeClassifier;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.bytedeco.javacv.*;
+import org.bytedeco.javacpp.*;
+import org.bytedeco.javacpp.indexer.*;
+import org.bytedeco.opencv.opencv_core.*;
+import org.bytedeco.opencv.opencv_imgproc.*;
+import org.bytedeco.opencv.opencv_calib3d.*;
+import org.bytedeco.opencv.opencv_objdetect.*;
+
+import static org.bytedeco.opencv.global.opencv_core.*;
+import static org.bytedeco.opencv.global.opencv_imgproc.*;
+import static org.bytedeco.opencv.global.opencv_calib3d.*;
+import static org.bytedeco.opencv.global.opencv_objdetect.*;
 
 
 public class FaceRecognition {
 
-    private final OpenCVFrameGrabber grabber;
+    private final FrameGrabber grabber;
+    private final OpenCVFrameConverter.ToMat converter;
     private final CascadeClassifier classifier;
 
-    public FaceRecognition() throws FrameGrabber.Exception {
-        grabber = new OpenCVFrameGrabber(0);
-        grabber.start();
-        grabber.setFrameRate(grabber.getFrameRate());
+    public FaceRecognition() throws IOException {
+        classifier = new CascadeClassifier(loadClassifier());
+        converter = new OpenCVFrameConverter.ToMat();
 
-        classifier = new CascadeClassifier();
-        classifier.load("face_cascade.xml");
+        grabber = FrameGrabber.createDefault(0);
+        grabber.start();
+    }
+
+    public String loadClassifier() throws IOException {
+        URL url = new URL("https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_alt.xml");
+        File file = Loader.cacheResource(url);
+        return file.getAbsolutePath();
     }
 
     public Frame grabFrame() throws FrameGrabber.Exception {
         return grabber.grab();
     }
 
-    public void dispose() {
-        try {
-            grabber.stop();
-        } catch (FrameGrabber.Exception e) {
-            e.printStackTrace();
-        }
+    public Mat convertFrame(Frame frame) {
+        return converter.convertToMat(frame);
     }
 
-    public static void main(String[] main) throws FrameGrabber.Exception {
-        try {
-            FaceRecognition face = new FaceRecognition();
-            Frame frame = face.grabFrame();
+    public static void main(String[] main) throws IOException {
+        FaceRecognition faceRecog = new FaceRecognition();
 
-            CanvasFrame canvasFrame = new CanvasFrame("Cam");
-            canvasFrame.setCanvasSize(frame.imageWidth, frame.imageHeight);
+        Frame frame = faceRecog.grabFrame();
+        Mat grabbedImage = faceRecog.convertFrame(frame);
+        int height = grabbedImage.rows();
+        int width = grabbedImage.cols();
 
-            while (canvasFrame.isVisible()) {
-                frame = face.grabFrame();
-                Frame grayFrame = null;
-                //Imgproc.cvtColor(frame, grayFrame, Imgproc.COLOR_BGR2GRAY);
+        // Objects allocated with `new`, clone(), or a create*() factory method are automatically released
+        // by the garbage collector, but may still be explicitly released by calling deallocate().
+        // You shall NOT call cvReleaseImage(), cvReleaseMemStorage(), etc. on objects allocated this way.
+        Mat grayImage = new Mat(height, width, CV_8UC1);
+        Mat rotatedImage = grabbedImage.clone();
 
-                canvasFrame.showImage(frame);
+        // CanvasFrame is a JFrame containing a Canvas component, which is hardware accelerated.
+        // It can also switch into full-screen mode when called with a screenNumber.
+        // We should also specify the relative monitor/camera response for proper gamma correction.
+        CanvasFrame canvasFrame = new CanvasFrame("Some Title", CanvasFrame.getDefaultGamma() / grabber.getGamma());
+
+        // Let's create some random 3D rotation...
+        Mat randomR = new Mat(3, 3, CV_64FC1),
+                randomAxis = new Mat(3, 1, CV_64FC1);
+        // We can easily and efficiently access the elements of matrices and images
+        // through an Indexer object with the set of get() and put() methods.
+        DoubleIndexer Ridx = randomR.createIndexer(),
+                axisIdx = randomAxis.createIndexer();
+        axisIdx.put(0, 0, 0, 0);
+        Rodrigues(randomAxis, randomR);
+        double f = (width + height) / 2.0;
+        Ridx.put(0, 2, Ridx.get(0, 2) * f);
+        Ridx.put(1, 2, Ridx.get(1, 2) * f);
+        Ridx.put(2, 0, Ridx.get(2, 0) / f);
+        Ridx.put(2, 1, Ridx.get(2, 1) / f);
+        System.out.println(Ridx);
+
+        // We can allocate native arrays using constructors taking an integer as argument.
+        Point hatPoints = new Point(3);
+
+        while (frame.isVisible() && (grabbedImage = converter.convert(grabber.grab())) != null) {
+            // Let's try to detect some faces! but we need a grayscale image...
+            cvtColor(grabbedImage, grayImage, CV_BGR2GRAY);
+            RectVector faces = new RectVector();
+            classifier.detectMultiScale(grayImage, faces);
+            long total = faces.size();
+            for (long i = 0; i < total; i++) {
+                Rect r = faces.get(i);
+                int x = r.x(), y = r.y(), w = r.width(), h = r.height();
+                rectangle(grabbedImage, new Point(x, y), new Point(x + w, y + h), Scalar.RED, 1, CV_AA, 0);
             }
-            canvasFrame.dispose();
-        } catch (Exception e) {
-            e.printStackTrace();
+
+            warpPerspective(grabbedImage, rotatedImage, randomR, rotatedImage.size());
+
+            Frame rotatedFrame = converter.convert(rotatedImage);
+            canvasFrame.showImage(rotatedFrame);
         }
+        canvasFrame.dispose();
+
     }
 
 }
